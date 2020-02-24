ae444af0db6d19585f0756584aef277341141171
ozilbbg11 <kfw19980807@gmail.com>
2020-02-25 03:40:52 +0800

    update

---
+++ django_project/blog/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .models import Post
+
+# Create your views here.
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
+
+def generate(request):
+    return render(request, 'blog/generate.html', {'title': 'generate'})
+
+def trend(request):
+    return render(request, 'blog/trend.html', {'title': 'trend'})
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Derek\PycharmProjects\fyp2.0\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
